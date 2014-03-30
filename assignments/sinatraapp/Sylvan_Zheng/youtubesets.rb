cf76a1d40f56b90e050b9dd873cd7bede3aaf7f4
Sylvan Zheng <squidgetx@gmail.com>
2014-03-29 20:46:57 -0400

    did a bunch of stuff

--- assignments/sinatraapp/Sylvan_Zheng/youtubesets.rb
+++ assignments/sinatraapp/Sylvan_Zheng/youtubesets.rb
@@ -9,50 +9,18 @@ configure do
 end
 
 
-helpers do
-  def randomvideo(set)
-    set.sample
-  end
 
-  #We're trying not to use the rest of these helpers any more, but they're here to make the pmj code below still work for comparison
-  def embedyoutube(videonumber)
-    %{
-    <body style="margin:0;">
-    <object height="100%" width="100%"><param name="movie" value="http://www.youtube.com/v/#{videonumber}&autoplay=1" /><embed height="100%" src="http://www.youtube.com/v/#{videonumber}&autoplay=1" type="application/x-shockwave-flash" width="100%"></embed></object>
-    </body>
-    }
-  end
-  def beyoncevideos
-    ["QczgvUDskk0", "VBmMU_iwe6U", "Vjw92oUduEM", "4m1EFMoRFvY", "FHp2KgyQUFk"]
-  end
-  def pmjvideos
-    ["pXYWDtXbBB0", "VBmCJEehYtU", "GZQJrM09jbU"]
-  end
-end
-
-##INDEX
-##Main Welcome Page
 get '/' do
   erb :index
-  #"<h1>Hi!</h1><h2>If you know the url you can play a random video from set of youtube videos!</h2>"
+
 end
 
+sets = Hash.new
 
-
-##Display the Video Sets, old-style where the videos are hard coded above
-get '/beyonce' do
-  #embedyoutube(randomvideo(beyoncevideos))
-  @videonumber = randomvideo(beyoncevideos)
-  erb :play
+get '/sets' do
+	@sets = sets
+	erb :sets
 end
-
-get '/pmj' do
-  embedyoutube(randomvideo(pmjvideos))
-end
-
-
-
-##New, RESTful code
 
 ##NEW page
 get '/sets/new' do
@@ -61,52 +29,43 @@ end
 
 ##Create page
 post "/sets" do
-  "Success!"
-  #This should include more
+  #"Success! #{params[:videolist]}"
+  sets[params[:setname]] = Hash.new
+  sets[params[:setname]][:name] = params[:setname]
+  sets[params[:setname]][:vidnums] = params[:videolist].split("\r\n")
+  @sets = sets
+  erb :sets
 end
 
-
-##not the RESTful new page with the form we want, but a parameters way to make a new video set
-get '/sets/new/:setname/:videonumber' do |setname, videonumber|
-  #setname = "pharrell"
-  #videonumber = "y6Sxv-sUYtM"
-  session[setname] = [videonumber]
-  "Video " + videonumber + "is now the only video in setname " + setname
+#display a set
+get '/sets/:name' do
+	@set = sets[params[:name]]
+	erb :showset
 end
 
-get '/sets/add/:setname/:videonumber' do |setname, videonumber|
-  session[setname] << videonumber
-  "Video " + videonumber + "has been added to set " + setname
+#play a set
+get '/sets/:name/play' do
+	@videonumber = sets[params[:name]][:vidnums].sample
+	erb :play
 end
 
-
-##Play the Pharrell video set - not necessary eventually, just for testing
-get '/sets/pharrell' do
-  @videonumber = randomvideo(session["pharrell"])
-  #@videonumber = "y6Sxv-sUYtM"
-  erb :play
+#edit a set
+get '/sets/:name/edit' do
+	@set = sets[params[:name]]
+	@text = sets[params[:name]][:vidnums].join("\n")
+	erb :edit
 end
 
-
-
-#Session page for troubleshooting the session
-get '/session' do
-  session[:sessiontestvariable] = 3.14
-  session.inspect
+#update a set
+post '/sets/:name' do
+	sets[params[:name]][:vidnums] = params[:videolist].split("\r\n")
+	@set = sets[params[:name]]
+	erb :showset
 end
 
-
-##example of sinatra input by url parameters
-##The ?var=1&var2=2&var3=3 style works too, but Sinatra can give us prettier url parameters
-get '/params/:idlol' do
-  params.inspect
-end
-
-get '/favorite/:fruit' do |fruit|
-  "My favorite fruit is the " + fruit.to_s
-  #params["fruit"] also works instead of fruit
-end
-
-get '/add/:num1/:num2' do |num1, num2|
-  ##your code here, as an example
+#delete a set
+delete '/sets/:name' do
+	sets.delete(params[:name])
+	@sets = sets
+	erb :sets
 end
