842970219233e0ab497dfb5d7826531ff2454ea6
Carsten Peterson <carsten.peterson@yale.edu>
2014-03-29 20:47:12 -0400

    Pretty much did everything

---
+++ assignments/sinatraapp/Carsten_Peterson/youtube_sets.rb
@@ -0,0 +1,60 @@
+require 'sinatra'
+require 'erb'
+require 'sinatra/reloader' if development?
+
+configure do
+	enable :sessions
+end
+
+sets = {}
+
+get '/' do
+	"hello"
+end
+
+get '/sets' do
+	@sets = sets.keys
+	erb :homepage
+end
+
+get '/sets/new' do
+	erb :new
+end
+
+post '/sets' do
+	sets[params[:setname]] = params[:videolist].split("\n")
+	"Success"
+end
+
+get '/sets/:name' do
+	@my_list = sets[params[:name]]
+	erb :list_videos
+end
+
+get '/sets/:name/play' do
+	@videonumber = sets[params[:name]].sample()
+	erb :play
+end
+
+get '/sets/:name/edit' do
+	@name = params[:name]
+	temp = ""
+	my_list = sets[params[:name]]
+	my_list.each do |entry|
+		temp += entry
+		temp += "\n"
+	end
+	@my_string = temp
+	erb :edit
+end
+
+post '/sets/:name' do
+	sets[params[:name]] = params[:videolist].split("\n")
+	@my_list = sets[params[:name]]
+	erb :list_videos
+end
+
+delete '/sets/:name' do
+	sets.delete(params[:name])
+	"Successfully removed the set"
+end
