68e043ea104908929d97f319e5ebf56f0b2531b4
Sylvan Zheng <squidgetx@gmail.com>
2014-04-08 15:37:22 -0400

    added playlist descritpion

--- assignments/sinatraapp/Sylvan_Zheng/youtubesets.rb
+++ assignments/sinatraapp/Sylvan_Zheng/youtubesets.rb
@@ -6,6 +6,8 @@ require "sinatra/reloader" if development?
 
 configure do
   enable :sessions
+  _method = true
+  use Rack::MethodOverride
 end
 
 
@@ -33,6 +35,7 @@ post "/sets" do
   sets[params[:setname]] = Hash.new
   sets[params[:setname]][:name] = params[:setname]
   sets[params[:setname]][:vidnums] = params[:videolist].split("\r\n")
+  sets[params[:setname]][:desc] = params[:description]
   @sets = sets
   erb :sets
 end
@@ -53,18 +56,22 @@ end
 get '/sets/:name/edit' do
 	@set = sets[params[:name]]
 	@text = sets[params[:name]][:vidnums].join("\n")
+	@desc = sets[params[:name]][:description]
 	erb :edit
 end
 
 #update a set
 post '/sets/:name' do
+	puts "Set updated1!!!!!!"
 	sets[params[:name]][:vidnums] = params[:videolist].split("\r\n")
+	sets[params[:name]][:desc] = params[:description]
 	@set = sets[params[:name]]
 	erb :showset
 end
 
 #delete a set
 delete '/sets/:name' do
+	puts("test!!!!!!!!!!!!!!!")
 	sets.delete(params[:name])
 	@sets = sets
 	erb :sets
