87ac06777ee453c725c684a09d18d4210fbbcca3
caseywatts <casey.s.watts@gmail.com>
2014-04-12 13:51:47 -0400

    add classassignment

---
+++ classexamples/day8/classassignment.rb
@@ -0,0 +1,21 @@
+#1. What is the first pokemon in our database? Our last one?
+
+
+#2. Find all pokemon that are GHOST type.
+
+
+#3. Are there more pokemon with even pokemon numbers who are GRASS type or who are FIRE type?
+
+
+#4. Try to defeat Charizard. Find a pokemon with special attack (sp_attack) greater than 78 and whose type is water.
+
+
+#5. Pokemon can no longer have over 100 health (hp). Update any pokemon who has health greater than 100 to have only 99 health (hp).
+
+
+#5. Make a new pokemon. What attributes are necessary? What attributes do you want to make sure you put in?
+
+
+#6. Everyone hates Jigglypuff. Remove that pokemon from the database (and existance!). Make sure it's not just removed from Rubyland, but also from Databaseland.
+
+
